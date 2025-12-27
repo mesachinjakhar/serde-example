7c4f0ac8502a02a0409c75e53b66325b612d0774
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-27 17:10:57 +0530

    select_all_fields

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-#[serde(rename(serialize = "ser_name", deserialize = "de_name"))]
+// #[serde(rename(serialize = "ser_name", deserialize = "de_name"))]
+// #[serde(rename_all(serialize = "UPPERCASE"))]
 pub struct Point {
     x: i32,
     y: i32
 }
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all_fields(serialize = "camelCase"))]
+enum Event {
+    UserCreated { user_id: i32, user_name: String },
+    UserDeleted { user_id: i32 },
+}
+
 
 fn main() {
     let point = Point {
         x: 1,
         y: 2
     };
+
+    let event = Event::UserCreated { user_id: 123, user_name: "mesachinjakhar".to_string() };
+    let event_serialized = serde_json::to_string(&event).unwrap();
+       println!("event serialized: {}", event_serialized);
     
     // convert struct into JSON string
     let serialized = serde_json::to_string(&point).unwrap();
