bb3c66cbf22307c18085b7cdb93131d691212fc5
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-27 16:00:41 +0530

    derive on struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    email: String,
+    username: String
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
