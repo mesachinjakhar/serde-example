01a72c604cce7d5dacf6aacb68d59dbd1a0bc349
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-27 16:04:37 +0530

    struct to json

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
-pub struct User {
-    email: String,
-    username: String
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Point {
+    x: i32,
+    y: i32
 }
 
-
 fn main() {
-    println!("Hello, world!");
+    let point = Point {
+        x: 1,
+        y: 2
+    };
+    
+    // convert struct into JSON string
+    let serilized = serde_json::to_string(&point).unwrap();
+
+    println!("serilized: {}", serilized);
 }
